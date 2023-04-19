229fcacd2b6cfbc4d72dff8982f5c66836a636b2
Anvit Dubey <73951320+anvbey@users.noreply.github.com>
2023-04-20 01:23:02 +0530

    64. Minimum Path Sum

---
+++ src/MinimumPathSum64.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i=1;i<n;i++)
+        {
+            grid[i][0] +=  grid[i-1][0];
+            System.out.println(grid[i][0]);
+        }
+        System.out.println();
+        for(int i=1;i<m;i++)
+        {
+            grid[0][i] +=  grid[0][i-1];
+            System.out.println(grid[0][i]);
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                grid[i][j] = grid[i][j] + Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+
+        return grid[n-1][m-1];
+    }
+}
